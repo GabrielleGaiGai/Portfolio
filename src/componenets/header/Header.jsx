70230b3656c7db45ab901f19fa57fae53df827b6
Xiangzhu Lu <x244lu@uwaterloo.ca>
2024-05-01 10:23:22 -0400

    Add the functionality "click anywhere else to close"

--- src/componenets/header/Header.jsx
+++ src/componenets/header/Header.jsx
@@ -2,44 +2,44 @@ import React, { useState } from 'react';
 import "./Header.css";
 
 function Header() {
-  const [Show_toggle, showMenu] = useState(false);
+  const [open, openMenu] = useState(false);
 
   return (
     <header className="header">
       <nav className="nav container">
 
-        <div className={Show_toggle ? "nav__menu show-menu" : "nav__menu"}>
+        <div className={open ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list">
             <li className='home'>
-              <a href="#home" className="nav__link">
+              <a href="#home" className="nav__link" onClick={() => openMenu(false)}>
                 <i className="uil uil-estate nav__icon"></i>
                 <span className='nav__text'> Home </span>
               </a>
             </li>
             
             <li className='about'>
-              <a href="#about" className="nav__link">
+              <a href="#about" className="nav__link" onClick={() => openMenu(false)}>
                 <i className="uil uil-user nav__icon"></i>
                 <span className='nav__text'> About </span>
               </a>
             </li>
 
             <li className='skills'>
-              <a href="#skills" className="nav__link">
+              <a href="#skills" className="nav__link" onClick={() => openMenu(false)}>
                 <i className="uil uil-file-info-alt nav__icon"></i>
                 <span className='nav__text'> Skills </span>
               </a>
             </li>
 
             <li className='projects'>
-              <a href="#projects" className="nav__link">
+              <a href="#projects" className="nav__link" onClick={() => openMenu(false)}>
                 <i className="uil uil-folder-open nav__icon"></i>
                 <span className='nav__text'> Projects </span>
               </a>
             </li>
 
             <li className='contact'>
-              <a href="#contact" className="nav__link">
+              <a href="#contact" className="nav__link" onClick={() => openMenu(false)}>
                 <i className="uil uil-message nav__icon"></i>
                 <span className='nav__text'> Contact </span>
               </a>
@@ -47,12 +47,12 @@ function Header() {
           </ul>
         </div>
 
-        <div className={Show_toggle ? "nav__close" : "nav__close hide-icon"} onClick={() => showMenu(!Show_toggle)}>
+        <div className={open ? "nav__close" : "nav__close hide-icon"} onClick={() => openMenu(false)}>
           <i className='uil uil-multiply'></i>
           <span> Close </span>
         </div>
 
-        <div className={Show_toggle ? "nav__toggle hide-icon" : "nav__toggle"} onClick={() => showMenu(!Show_toggle)}>
+        <div className={open ? "nav__toggle hide-icon" : "nav__toggle"} onClick={() => openMenu(true)}>
           <i className="uil uil-apps"></i>
           <span> Menu </span>
         </div>
--- src/componenets/projects/Item.jsx
+++ src/componenets/projects/Item.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function validate_link(link, title) {
     if (link.length === 0) {
@@ -15,6 +15,19 @@ function validate_link(link, title) {
 
 function Item({ item }) {
     const [toggleState, setToggleState] = useState(false);
+    const detailBox = useRef(0);
+    const openProject = useRef(0);
+
+    useEffect(() => {
+        function clickAnyelse(event) {
+            if (!openProject.current.contains(event.target) &&
+                detailBox.current.className.includes("projectItem__box-active") &&
+                !detailBox.current.contains(event.target)) {
+                setToggleState(false);
+            }
+        }
+        document.body.addEventListener('click', clickAnyelse);
+    }, [])
 
     return (
         <div className="projectItem box">
@@ -24,13 +37,13 @@ function Item({ item }) {
             <div className="projectItem__intro">
                 {item.intro}
             </div>
-            <div className="projectItem__more" onClick={() => setToggleState(true)}>
+            <div className="projectItem__more" onClick={() => setToggleState(true)} ref={openProject}>
                 View More
                 <i class="uil uil-arrow-right projectItem__icon"></i>
             </div>
 
             <div className={toggleState ? "projectItem__data projectItem__data-active" : "projectItem__data"}>
-                <div className="projectItem__box box">
+                <div className={toggleState ? "projectItem__box box projectItem__box-active" : "projectItem__box box"} ref={detailBox}>
                     <div className="projectItem__detail">
                         <i className="uil uil-times projects__close" onClick={() => setToggleState(false)}></i>
                         <h3 className="projectItem__detail__title">
